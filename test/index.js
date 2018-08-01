c5a0ff5ccd3d32fe5e343c1125010615f257b483
Aleš Menzel <alesmenzel@gmail.com>
2018-08-01 22:28:08 +0200

    adds colorizer function based on namespace

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 const chalk = require("chalk");
 const util = require("util");
+const crypto = require("crypto");
 
-process.env.CHURCHILL_DEBUG = "*";
-// process.env.CHURCHILL_DEBUG_LEVEL = "info";
+// process.env.CHURCHILL_DEBUG = "worker:a:*"; // Log only namespaces matching worker:a:*
+// process.env.CHURCHILL_DEBUG_LEVEL = "debug"; // Log only verbose and above
 
 const churchill = require("../src/churchill");
 
@@ -15,25 +16,44 @@ const colors = {
   silly: "gray"
 };
 
-const colorizeLevel = level => chalk[colors[level.toLowerCase()]](level);
+// Colorize the log level
+const colorizeByLevel = level => chalk[colors[level.toLowerCase()]](level);
 
+const md5 = str =>
+  crypto
+    .createHash("md5")
+    .update(str)
+    .digest("hex");
+
+const colorizeByNamespace = namespace => {
+  const hex = md5(md5(namespace)).slice(0, 6);
+
+  return chalk.hex(`#${hex}`);
+};
+
+// Setup the logger - returns a createLogger function
 const createLogger = churchill({
-  transports: [new churchill.Console({ level: "verbose" })],
+  transports: [new churchill.Console({ level: "verbose" })], // Transports - must have a log() function
+  // Custom formatter function
   format: info => {
     const { namespace, level, timestamp, ms, args } = info;
 
-    // eslint-disable-next-line
-    return `[${timestamp.toISOString()}] ${chalk.blue(namespace)} ${colorizeLevel(level.toUpperCase())} ${util.format(...args)} +${ms}ms\n`
+    const namespaceColor = colorizeByNamespace(namespace);
+
+    const time = namespaceColor(timestamp.toISOString());
+    const nmsp = namespaceColor(namespace);
+    const lvl = colorizeByLevel(level.toUpperCase());
+    const msg = namespaceColor(util.format(...args));
+    const elapsed = namespaceColor(`+${ms}ms`);
+
+    return `${nmsp} [${time}] ${lvl} ${msg} ${elapsed}\n`;
   }
 });
 
-const logger = createLogger("namespace:test");
+const loggerA = createLogger("worker:a"); // namespace
+const loggerB = createLogger("worker:b"); // namespace
+const loggerC = createLogger("worker:c"); // namespace
 
-logger.silly("test", { a: 5 });
-logger.debug("test", { a: 5 });
-logger.verbose("test", { a: 5 });
-logger.info("test", { a: 5 });
-logger.warn("test", { a: 5 });
-logger.error("test", { a: 5 }, new Error("Wtf!"));
-
-logger("error", "test", { abc: 546 });
+loggerA.info("test", { metadata: "some info" });
+loggerB.info("test", { metadata: "some info" });
+loggerC.info("test", { metadata: "some info" });
