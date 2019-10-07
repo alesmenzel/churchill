557ca1ac35975026fc97301f525b8156e3e6105a
Ales Menzel <ales.menzel@socialbakers.com>
2019-10-07 22:17:33 +0200

    elastic transport, stream example

--- src/transports/index.js
+++ src/transports/index.js
@@ -2,5 +2,6 @@ const Console = require("./console");
 const File = require("./file");
 const HTTP = require("./http");
 const Stream = require("./stream");
+const Elastic = require("./elastic");
 
-module.exports = { Console, File, HTTP, Stream };
+module.exports = { Console, File, HTTP, Stream, Elastic };
--- test/acceptance/console.test.js
+++ test/acceptance/console.test.js
@@ -1,3 +1,3 @@
 describe("console", () => {
-  it("acceptance");
+  it.todo("acceptance");
 });
